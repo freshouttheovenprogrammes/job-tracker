6933b54c2a931ecf3a511c22fc795505de27af25
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-27 09:08:50 -0600

    create jobs test all green

--- spec/features/user_can_create_job_spec.rb
+++ spec/features/user_can_create_job_spec.rb
@@ -10,6 +10,16 @@ describe "As a user" do
       click_link "Add Job"
 
       expect(current_path).to eq(new_company_job_path(company))
+
+      fill_in "job[title]", with: "Job1"
+      fill_in "job[description]", with: "Its a Job!"
+      fill_in "job[level_of_interest]", with: 90
+      fill_in "job[city]", with: "Denver"
+
+      click_on "Create Job"
+      job = company.jobs.first
+      expect(page).to have_content "You created Job1 at #{company.name}"
+      expect(current_path).to eq company_job_path(company, job)
     end
   end
 end
